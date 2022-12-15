3ecc7b7405a94d4e3f450aeb5cb57415373fae60
Carlos <carlos.barahona.t@usach.cl>
2022-12-15 00:27:02 -0300

    Day 14
    Pretty proud of this one, came out clean overall I'd say. My part 1 solution made the part 2 relatively easy.

--- src/day_manager/day13.rs
+++ src/day_manager/day13.rs
@@ -17,21 +17,33 @@ pub fn day_13_main(input_file: Option<String>) {
         file_path.clone()
     ));
     println!("\n{}\n", input);
-    
+
     println!("-------------------------Test start---------------");
 
     let test_str = "[[0,[[]]],[1],[2]";
     let test_str2 = "[[2,[]]]";
     println!("Parsing:\n{}\n{}\n", test_str, test_str2);
 
-    let test_str_parsed = parse_signal_side(test_str.strip_prefix("[").unwrap().strip_suffix("]").unwrap());
-    let test_str_parsed2 = parse_signal_side(test_str2.strip_prefix("[").unwrap().strip_suffix("]").unwrap());
+    let test_str_parsed = parse_signal_side(
+        test_str
+            .strip_prefix("[")
+            .unwrap()
+            .strip_suffix("]")
+            .unwrap(),
+    );
+    let test_str_parsed2 = parse_signal_side(
+        test_str2
+            .strip_prefix("[")
+            .unwrap()
+            .strip_suffix("]")
+            .unwrap(),
+    );
 
     println!("\nstr 1 parsed: {:?}", test_str_parsed);
     println!("\nstr 2 parsed: {:?}", test_str_parsed2);
 
     println!("-------------------------Test end----------------");
-/* 
+    /*
     let mut signals: Vec<Vec<SignalElement>> = vec![];
     for line in input.lines() {
         if line.is_empty() {
@@ -48,109 +60,113 @@ pub fn day_13_main(input_file: Option<String>) {
 
 // gets a line with a signal side and return a vec with each element (a num or another list)
 fn parse_signal_side(signal: &str) -> Vec<SignalElement> {
-
     let mut items: Vec<SignalElement> = vec![];
     let mut bracket_count = 0;
 
     for (pos, char) in signal.chars().enumerate() {
-
         match char {
             '[' => {
-                println!("\nBRACKET COUNT!!!!!!!: {} -- slice left: {}", bracket_count, &signal[pos..]);
+                println!(
+                    "\nBRACKET COUNT!!!!!!!: {} -- slice left: {}",
+                    bracket_count,
+                    &signal[pos..]
+                );
                 if bracket_count == 0 {
                     println!("\nNested list detected! starting recursion:");
                     items.push(deconstruct_signal_list(&signal[pos..], &mut vec![], 0).unwrap());
                 }
                 bracket_count += 1;
-            },
+            }
             ']' => {
                 bracket_count -= 1;
-            },
+            }
             ',' => {
                 continue;
             }
             _ => {
                 if bracket_count == 0 {
-                    items.push(SignalElement::Number(char.to_string().parse().expect(&format!("Could not parase number in input: {}", char))));
+                    items.push(SignalElement::Number(
+                        char.to_string()
+                            .parse()
+                            .expect(&format!("Could not parase number in input: {}", char)),
+                    ));
                 }
             }
         }
     }
 
     items
-    
 }
 
 // i can't believe this worked
 // gets a list of signal types in a string and recursively builds it into the proper SignalElement::List(_) enum using recursion
 
-
 // TODO: change return to Option<SignalElement> to be able to return None and avoid the double nested lists (i think)
 
-
-fn deconstruct_signal_list(partial_signal: &str, final_vec: &mut Vec<SignalElement>, bracket_count: usize) -> Option<SignalElement> {
-
-
+fn deconstruct_signal_list(
+    partial_signal: &str,
+    final_vec: &mut Vec<SignalElement>,
+    bracket_count: usize,
+) -> Option<SignalElement> {
     //println!("Current slice: {} -- Bracket count: {}", partial_signal, bracket_count);
     println!("Received: {:?} with slice {}", final_vec, partial_signal);
     match partial_signal.chars().next() {
-        Some(c) => {
-            match c {
-                '[' => {
-                    
-                    if let Some(signal_part) = deconstruct_signal_list(&partial_signal[1..], &mut vec![], bracket_count + 1) {
-                        
-                        println!("bracket count: {}", bracket_count);
-                        println!("Pushing: {:?}", signal_part);
-                        final_vec.push(signal_part);
-                    }
-
-                    println!("Returning [: {:?}", SignalElement::List( final_vec.to_vec()));
-                    if final_vec.is_empty() {
-                        return Some(SignalElement::List(vec![]));
-                    }
-                    else if bracket_count != 0 {
-                        return Some(SignalElement::List( final_vec.to_vec()));
-                    }
-                    else {
-                        println!("VEC WHEN BCOUNT ISNT 0: {:?}", final_vec);
-                        return Some(final_vec.to_vec()[0].clone());
-                    }
-                },
-                ']' => {
+        Some(c) => match c {
+            '[' => {
+                if let Some(signal_part) =
+                    deconstruct_signal_list(&partial_signal[1..], &mut vec![], bracket_count + 1)
+                {
                     println!("bracket count: {}", bracket_count);
-                    println!("VEC IN ]: {:?}", final_vec);
-                    if bracket_count != 1 {
-                        deconstruct_signal_list(&partial_signal[1..], final_vec, bracket_count - 1);
-                        println!("Retuning from ]");
-                        return None;
-                    }
-                    else {
-                        println!("FINISHED RECURSION!!!!!!!!");
-                        return None;
-                    }
+                    println!("Pushing: {:?}", signal_part);
+                    final_vec.push(signal_part);
+                }
 
-                    
-                },
-                ',' => {
-                    deconstruct_signal_list(&partial_signal[1..], final_vec, bracket_count);
-                    println!("Returning ,: {:?}", SignalElement::List(final_vec.to_vec()));
+                println!("Returning [: {:?}", SignalElement::List(final_vec.to_vec()));
+                if final_vec.is_empty() {
+                    return Some(SignalElement::List(vec![]));
+                } else if bracket_count != 0 {
                     return Some(SignalElement::List(final_vec.to_vec()));
-                },
-                _ => {
-                    final_vec.push(SignalElement::Number(c.to_string().parse().expect(&format!("Could not parase number in input: {}", c))));
-                    deconstruct_signal_list(&partial_signal[1..], final_vec, bracket_count);
-                    println!("Returning {}: {:?}", c, SignalElement::List(final_vec.to_vec()));
-                    return Some(SignalElement::List(final_vec.to_vec()));
+                } else {
+                    println!("VEC WHEN BCOUNT ISNT 0: {:?}", final_vec);
+                    return Some(final_vec.to_vec()[0].clone());
                 }
             }
-
+            ']' => {
+                println!("bracket count: {}", bracket_count);
+                println!("VEC IN ]: {:?}", final_vec);
+                if bracket_count != 1 {
+                    deconstruct_signal_list(&partial_signal[1..], final_vec, bracket_count - 1);
+                    println!("Retuning from ]");
+                    return None;
+                } else {
+                    println!("FINISHED RECURSION!!!!!!!!");
+                    return None;
+                }
+            }
+            ',' => {
+                deconstruct_signal_list(&partial_signal[1..], final_vec, bracket_count);
+                println!("Returning ,: {:?}", SignalElement::List(final_vec.to_vec()));
+                return Some(SignalElement::List(final_vec.to_vec()));
+            }
+            _ => {
+                final_vec.push(SignalElement::Number(
+                    c.to_string()
+                        .parse()
+                        .expect(&format!("Could not parase number in input: {}", c)),
+                ));
+                deconstruct_signal_list(&partial_signal[1..], final_vec, bracket_count);
+                println!(
+                    "Returning {}: {:?}",
+                    c,
+                    SignalElement::List(final_vec.to_vec())
+                );
+                return Some(SignalElement::List(final_vec.to_vec()));
+            }
         },
         None => {
             // this should not run ever with a good input
             println!("THIS RUNS?????");
-            return None
-        },
+            return None;
+        }
     }
-
 }
--- src/day_manager/day14.rs
+++ src/day_manager/day14.rs
@@ -1,15 +1,349 @@
-use std::fs;
+use std::{collections::HashSet, fs, thread::sleep, time::Duration};
+
+#[derive(Eq, PartialEq, Debug)]
+struct CaveMap {
+    used_spaces: HashSet<Point>,
+    max_x: i32,
+    max_y: i32,
+    min_x: i32,
+    min_y: i32,
+}
+
+#[derive(Eq, Hash, PartialEq, Debug, Clone)]
+struct Point {
+    kind: PointKind,
+    x: i32,
+    y: i32,
+}
+
+#[derive(Eq, Hash, PartialEq, Debug, Clone)]
+enum PointKind {
+    Sand,
+    Rock,
+    Source,
+}
 
 pub fn day_14_main(input_file: Option<String>) {
     // https://adventofcode.com/2022/day/14
     let file_path = match input_file {
         Some(file) => file,
-        None => "inputs/placeholder.txt".to_owned(),
+        None => "inputs/day_14_sand.txt".to_owned(),
     };
     let input = fs::read_to_string(&file_path).expect(&format!(
         "\n\nCould not read or find file: {}\n\n",
         file_path.clone()
     ));
     println!("\n{}\n", input);
-    todo!();
+
+    let mut cave = CaveMap {
+        used_spaces: HashSet::new(),
+        max_x: 500,
+        max_y: 0,
+        min_x: 500,
+        min_y: 0,
+    };
+
+    for line in input.lines() {
+        // yeah
+        let line_points = line
+            .split(" -> ")
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|s| s.split(',').collect::<Vec<&str>>())
+            .collect::<Vec<Vec<&str>>>()
+            .iter()
+            .map(|p| p.iter().map(|x| x.parse().unwrap()).collect())
+            .collect::<Vec<Vec<i32>>>();
+
+        //println!("{:?}", line_points);
+
+        if line_points.len() > 1 {
+            for i in 0..line_points.len() - 1 {
+                //println!("CASE: {:?} vs {:?}", line_points[i], line_points[i+1]);
+                // if Y increases, do Y1..Y2
+                if line_points[i][1] < line_points[i + 1][1]
+                    && line_points[i][0] == line_points[i + 1][0]
+                {
+                    for y in line_points[i][1]..=line_points[i + 1][1] {
+                        (cave.max_x, cave.min_x, cave.max_y, cave.min_y) = get_min_max_nums(
+                            cave.max_x,
+                            cave.max_y,
+                            cave.min_x,
+                            cave.min_y,
+                            line_points[i][0],
+                            y,
+                        );
+                        cave.used_spaces.insert(Point {
+                            x: line_points[i][0],
+                            y,
+                            kind: PointKind::Rock,
+                        });
+                    }
+                }
+                // if Y decreases, do Y2..Y1
+                else if line_points[i][1] > line_points[i + 1][1]
+                    && line_points[i][0] == line_points[i + 1][0]
+                {
+                    for y in line_points[i + 1][1]..=line_points[i][1] {
+                        (cave.max_x, cave.min_x, cave.max_y, cave.min_y) = get_min_max_nums(
+                            cave.max_x,
+                            cave.max_y,
+                            cave.min_x,
+                            cave.min_y,
+                            line_points[i][0],
+                            y,
+                        );
+                        cave.used_spaces.insert(Point {
+                            x: line_points[i][0],
+                            y,
+                            kind: PointKind::Rock,
+                        });
+                    }
+                }
+                // if X increases, do X1..X2
+                else if line_points[i][0] < line_points[i + 1][0]
+                    && line_points[i][1] == line_points[i + 1][1]
+                {
+                    for x in line_points[i][0]..=line_points[i + 1][0] {
+                        (cave.max_x, cave.min_x, cave.max_y, cave.min_y) = get_min_max_nums(
+                            cave.max_x,
+                            cave.max_y,
+                            cave.min_x,
+                            cave.min_y,
+                            x,
+                            line_points[i][1],
+                        );
+                        cave.used_spaces.insert(Point {
+                            x,
+                            y: line_points[i][1],
+                            kind: PointKind::Rock,
+                        });
+                    }
+                }
+                // if X decreases, do X2..X1
+                else if line_points[i][0] > line_points[i + 1][0]
+                    && line_points[i][1] == line_points[i + 1][1]
+                {
+                    for x in line_points[i + 1][0]..=line_points[i][0] {
+                        (cave.max_x, cave.min_x, cave.max_y, cave.min_y) = get_min_max_nums(
+                            cave.max_x,
+                            cave.max_y,
+                            cave.min_x,
+                            cave.min_y,
+                            x,
+                            line_points[i][1],
+                        );
+                        cave.used_spaces.insert(Point {
+                            x,
+                            y: line_points[i][1],
+                            kind: PointKind::Rock,
+                        });
+                    }
+                }
+                // the input is broken
+                else {
+                    println!("THE INPUT IS BROKEN or maybe it's me idk");
+                    panic!("Please check the logic here, or the input");
+                }
+            }
+        }
+    }
+
+    let sand_source = Point {
+        kind: PointKind::Source,
+        x: 500,
+        y: 0,
+    };
+    cave.used_spaces.insert(sand_source.clone());
+
+    //println!("{:?}", cave);
+    println!("Rocks processed! This is the map: with the sand source:");
+
+    // now it's time for the sand to fall
+    // keep executing until the used spaces contain the spot below the source, this means that no more sand can fall
+    println!("Sand is starting to fall:");
+    let mut stalled = false;
+    let inf_floor = true; // for part 2, just set to false to see part 1
+    let mut sand_counter = 0;
+    while !stalled {
+        // create sand, do not add to actual cave until it can't move anymore
+        let mut sand_falling = Point {
+            kind: PointKind::Sand,
+            x: sand_source.x,
+            y: sand_source.y,
+        };
+
+        // sand falling loop
+        loop {
+            //println!("Curr sand pos: {} {}", sand_falling.x, sand_falling.y);
+            // too slow for the full input
+            //print_cave(&cave, &sand_falling, inf_floor);
+
+            if sand_falling.y > cave.max_y + 1
+                || cave.used_spaces.contains(&Point {
+                    kind: PointKind::Sand,
+                    x: sand_source.x,
+                    y: sand_source.y,
+                })
+            {
+                print_cave(&cave, &sand_falling, inf_floor);
+                stalled = true;
+                break;
+            }
+
+            // check the 3 spaces below the sand and act accordingly
+            match count_occupied_below(&cave, &sand_falling, inf_floor) {
+                1 => {
+                    sand_falling.y += 1;
+                    sand_falling.x -= 1;
+                }
+                2 => {
+                    sand_falling.y += 1;
+                    sand_falling.x += 1;
+                }
+                3 => {
+                    cave.used_spaces.insert(sand_falling);
+                    sand_counter += 1;
+                    break;
+                }
+                _ => {
+                    sand_falling.y += 1;
+                }
+            }
+        }
+    }
+
+    println!(
+        "Sand has stopped falling! The final sand block count is {}",
+        sand_counter
+    );
+}
+
+fn get_min_max_nums(
+    og_max_x: i32,
+    og_max_y: i32,
+    og_min_x: i32,
+    og_min_y: i32,
+    new_x: i32,
+    new_y: i32,
+) -> (i32, i32, i32, i32) {
+    (
+        og_max_x.max(new_x),
+        og_min_x.min(new_x),
+        og_max_y.max(new_y),
+        og_min_y.min(new_y),
+    )
+}
+
+fn print_cave(cave: &CaveMap, sand_falling: &Point, inf_floor: bool) {
+    let width_offset;
+    if inf_floor {
+        width_offset = 10;
+    } else {
+        width_offset = 2;
+    }
+
+    sleep(Duration::from_millis(50));
+    for j in cave.min_y..=cave.max_y + 1 {
+        for i in cave.min_x - width_offset..=cave.max_x + width_offset {
+            if cave.used_spaces.contains(&Point {
+                x: i,
+                y: j,
+                kind: PointKind::Rock,
+            }) {
+                print!("██");
+            } else if cave.used_spaces.contains(&Point {
+                x: i,
+                y: j,
+                kind: PointKind::Source,
+            }) {
+                print!("◥◤")
+            } else if cave.used_spaces.contains(&Point {
+                x: i,
+                y: j,
+                kind: PointKind::Sand,
+            }) || sand_falling.x == i && sand_falling.y == j
+            {
+                print!("**");
+            } else {
+                print!("  ");
+            }
+        }
+        println!("");
+    }
+
+    // extra line if the inf floor is active
+    if inf_floor {
+        for _ in cave.min_x - width_offset..=cave.max_x + width_offset {
+            print!("██");
+        }
+    }
+
+    println!("");
+}
+
+// always the same order: down, downleft, downright, so...
+// all 3 used -> 3
+// down and downleft -> 2
+// down -> 1
+// none -> 0
+fn count_occupied_below(cave: &CaveMap, sand_falling: &Point, inf_floor: bool) -> i32 {
+    // if the inf floor is active, just return 3 once it's reached
+    if inf_floor && cave.max_y + 1 == sand_falling.y {
+        return 3;
+    }
+
+    let mut count = 0;
+    // check below
+    if cave.used_spaces.contains(&Point {
+        kind: PointKind::Rock,
+        x: sand_falling.x,
+        y: sand_falling.y + 1,
+    }) || cave.used_spaces.contains(&Point {
+        kind: PointKind::Sand,
+        x: sand_falling.x,
+        y: sand_falling.y + 1,
+    }) || cave.used_spaces.contains(&Point {
+        kind: PointKind::Source,
+        x: sand_falling.x,
+        y: sand_falling.y + 1,
+    }) {
+        count += 1;
+
+        // check down left
+        if cave.used_spaces.contains(&Point {
+            kind: PointKind::Rock,
+            x: sand_falling.x - 1,
+            y: sand_falling.y + 1,
+        }) || cave.used_spaces.contains(&Point {
+            kind: PointKind::Sand,
+            x: sand_falling.x - 1,
+            y: sand_falling.y + 1,
+        }) || cave.used_spaces.contains(&Point {
+            kind: PointKind::Source,
+            x: sand_falling.x - 1,
+            y: sand_falling.y + 1,
+        }) {
+            count += 1;
+
+            // check rown right
+            if cave.used_spaces.contains(&Point {
+                kind: PointKind::Rock,
+                x: sand_falling.x + 1,
+                y: sand_falling.y + 1,
+            }) || cave.used_spaces.contains(&Point {
+                kind: PointKind::Sand,
+                x: sand_falling.x + 1,
+                y: sand_falling.y + 1,
+            }) || cave.used_spaces.contains(&Point {
+                kind: PointKind::Source,
+                x: sand_falling.x + 1,
+                y: sand_falling.y + 1,
+            }) {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
